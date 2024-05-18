96f16e8ca0582431d17c6c8348fa5f116437be46
Yaroslav Shutov <yarshutov@yandex.ru>
2024-05-18 19:24:27 +0300

    small auth svc refactor

--- auth/go.mod
+++ auth/go.mod
@@ -3,27 +3,26 @@ module example.com/main
 go 1.22.0
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
-	google.golang.org/grpc v1.62.1
+	github.com/nyaruka/phonenumbers v1.3.5
+	golang.org/x/crypto v0.23.0
+	google.golang.org/grpc v1.64.0
 	google.golang.org/protobuf v1.33.0
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/nyaruka/phonenumbers v1.3.5 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240304212257-790db918fca8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- auth/src/internal/server/server.go
+++ auth/src/internal/server/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	"example.com/main/src/gen/go/sso"
+	"example.com/main/src/internal/client"
 	"example.com/main/src/internal/service"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -15,12 +16,17 @@ import (
 type GRPCServer struct {
 	gRPCServer *grpc.Server
 	sso.UnimplementedAuthServer
-	AuthService *service.AuthService
+	authService    *service.AuthService
+	userMgmtClient *client.UserMgmtGRPCClient
 }
 
-func New(authService *service.AuthService) *GRPCServer {
+func New(authService *service.AuthService, userMgmtClient *client.UserMgmtGRPCClient) *GRPCServer {
 	gRPCServer := grpc.NewServer()
-	g := &GRPCServer{gRPCServer: gRPCServer, AuthService: authService}
+	g := &GRPCServer{
+		gRPCServer:     gRPCServer,
+		authService:    authService,
+		userMgmtClient: userMgmtClient,
+	}
 	sso.RegisterAuthServer(gRPCServer, g)
 	return g
 }
@@ -32,16 +38,17 @@ func (s *GRPCServer) Start(l net.Listener) error {
 }
 
 func (s *GRPCServer) Register(ctx context.Context, req *sso.RegisterRequest) (*sso.RegisterResponse, error) {
-	token, err := s.AuthService.Register(req.GetLogin(), req.GetUsername(), req.GetPassword())
+	token, err := s.authService.Register(req.GetLogin(), req.GetUsername(), req.GetPassword())
 	if err != nil {
 		slog.Error(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+	s.userMgmtClient.PerformAddUser(ctx, req.GetLogin(), req.GetUsername())
 	return &sso.RegisterResponse{Token: token}, nil
 }
 
 func (s *GRPCServer) Login(ctx context.Context, req *sso.LoginRequest) (*sso.LoginResponse, error) {
-	token, err := s.AuthService.Login(req.GetLogin(), req.GetPassword())
+	token, err := s.authService.Login(req.GetLogin(), req.GetPassword())
 	if err != nil {
 		slog.Error(err.Error())
 		return nil, status.Error(codes.Unauthenticated, err.Error())
@@ -50,7 +57,7 @@ func (s *GRPCServer) Login(ctx context.Context, req *sso.LoginRequest) (*sso.Log
 }
 
 func (s *GRPCServer) Authorize(ctx context.Context, req *sso.AuthorizeRequest) (*sso.AuthorizeResponse, error) {
-	err := s.AuthService.Authorize(req.GetToken())
+	err := s.authService.Authorize(req.GetToken())
 	if err != nil {
 		slog.Error(err.Error())
 		return nil, status.Error(codes.PermissionDenied, err.Error())
