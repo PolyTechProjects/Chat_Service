c3afb45e2e6707ebc7e964516c45ee8f8728b0e3
Yaroslav Shutov <yarshutov@yandex.ru>
2024-05-22 19:21:33 +0300

    scripts to manage infrastructure; up and stop databases, redis, seaweedfs

---
+++ infra_start.sh
@@ -0,0 +1,14 @@
+cd auth/ && docker compose up auth_db -d && cd ../
+cd user-mgmt/ && docker compose up user_mgmt_db -d && cd ../
+cd chat-app/ && docker compose up chat_db -d && cd ../
+cd media-handler/ && docker compose up media_db seaweedfs_master seaweedfs_volume1 seaweedfs_volume2 redis -d && cd../
+echo '----------------------------------'
+echo 'AUTH_POSTGRES' $(docker ps | grep -i "auth_db" | awk '{print $1}')
+echo 'USER_MGMT_POSTGRES' $(docker ps | grep -i "user_mgmt_db" | awk '{print $1}')
+echo 'CHAT_POSTGRES' $(docker ps | grep -i "chat_db" | awk '{print $1}')
+echo 'MEDIA_HANDLER_POSTGRES' $(docker ps | grep -i "media_db" | awk '{print $1}')
+echo 'MEDIA_HANDLER_SEAWEEDFS_MASTER' $(docker ps | grep -i "seaweedfs_master" | awk '{print $1}')
+echo 'MEDIA_HANDLER_SEAWEEDFS_VOLUME_1' $(docker ps | grep -i "seaweedfs_volume1" | awk '{print $1}')
+echo 'MEDIA_HANDLER_SEAWEEDFS_VOLUME_2' $(docker ps | grep -i "seaweedfs_volume2" | awk '{print $1}')
+echo 'REDIS' $(docker ps | grep -i "redis" | awk '{print $1}')
+echo '----------------------------------'
---
+++ infra_stop.sh
@@ -0,0 +1,5 @@
+cd auth/ && docker compose stop auth_db && cd ../
+cd user-mgmt/ && docker compose stop user_mgmt_db && cd ../
+cd chat-app/ && docker compose stop chat_db && cd ../
+cd media-handler/ && docker compose stop media_db seaweedfs_master seaweedfs_volume1 seaweedfs_volume2 redis && cd ../
+docker ps
